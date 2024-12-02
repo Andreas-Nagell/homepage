104840c26f0e1c9dcbd6aaa647261d107161d210
Andreas-Nagell <andreas.nagel@uni-tuebingen.de>
2024-12-02 11:56:09 +0100

    Update nagel-andreas.mjs

--- src/.vitepress/data/persons/nagel-andreas.mjs
+++ src/.vitepress/data/persons/nagel-andreas.mjs
@@ -3,6 +3,9 @@ import { TeamID, definePerson } from '../..';
 export default definePerson({
     avatar: '/images/persons/nagel-andreas.png',
     name: 'Andreas Nagel',
+    email: 'andreas.nagel@uni-tuebingen.de',
+    phone: '+49 7071 29 70449',
+    address: 'Sand 14, 72076 Tübingen',
     role: 'Systemadministrator',
     team: TeamID.ABI,
     socialLinks: [],
